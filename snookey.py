1d4873d1eca14efec65fa36277725318c9badaae
Spikeedoo <spikeedoo14@gmail.com>
2020-03-22 00:56:11 -0500

    Added example images + updated comments

--- snookey.py
+++ snookey.py
@@ -9,8 +9,10 @@ callback = "http://localhost:65010/callback"
 state = "SNOOKEY"
 request_url = "https://www.reddit.com/api/v1/authorize?client_id=%s&response_type=%s&redirect_uri=%s&scope=%s&state=%s" % (client_id, response_type, callback, scope, state)
 
+#Open browser to get access token
 webbrowser.open(request_url, new=0)
 
+#Get user input
 print("HINT: Your code can be found on the localhost callback page.")
 user_token = input("Please enter your access token: ")
 full_token = "Bearer " + user_token
@@ -21,16 +23,19 @@ broadcast_endpoint = "https://strapi.reddit.com/r/%s/broadcasts?title=%s" % (sub
 
 payload = {}
 headers = {
-  'User-Agent': 'SnooKey/0.1 by u/Spikeedoo',
+  'User-Agent': 'Project SnooKey/0.1',
   'Authorization': full_token
 }
 
+# Request broadcast slot
 token_req = requests.request("POST", url=broadcast_endpoint, headers=headers, data=payload)
 
 if token_req.status_code == 200:
+  # Success!  Stream prepped
   response = token_req.json()
   print("YOUR STREAMER KEY: " + response["data"]["streamer_key"])
   print("YOU ARE LIVE: " + response["data"]["post"]["outboundLink"]["url"])
 else:
+  # Failed
   print("ERROR CODE " + str(token_req.status_code))
   print("The reddit servers said \"NONE SHALL PASS\"!  Your access token may be invalid... Please try again")
